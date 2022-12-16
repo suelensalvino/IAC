652c95e9b7685892cf91f288fcebb9c94ca2c551
BernardoFOFG <bernardofofg@gmail.com>
2022-12-16 02:14:52 -0300

    feat: adding func svelte slot for children to reuse the component

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,3 +1,3 @@
-<button type="submit" class="w-full bg-green-800 py-3 px-4 text-white text-lg rounded font-semibold">
-    Entrar na plataforma
+<button type="submit" class="w-full bg-green-800 py-3 px-4 text-white text-lg rounded font-semibold hover:bg-green-700 transition-all">
+    <slot name="button-child"></slot>
 </button>
