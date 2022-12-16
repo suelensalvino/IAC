00ddcc2264081ed33d739908c19f3b7db91724fa
BernardoFOFG <bernardofofg@gmail.com>
2022-12-16 02:20:34 -0300

    news

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,8 @@
 <script>
-  import SignUp from "./routes/SignUp.svelte";
-  import SignIn from "./routes/SignIn.svelte";
-
-  import LogoIFPE from "./assets/img/Logo_IFPE.svg";
-  import LogoIAC from "./assets/img/Logo_IAC.svg";
-  import { Link, Route, Router } from "svelte-navigator";
+  // @ts-nocheck
+  import Container from "./routes/Container.svelte";
 </script>
 
-<Router>
-  <main class="bg-gray-800">
-    <div
-      class="flex flex-col items-center justify-center w-[400px] h-screen mx-auto my-0"
-    >
-      <header class="flex flex-col">
-        <div class="flex">
-          <img src={LogoIFPE} alt="Logo do instituo federal" />
-          <img src={LogoIAC} alt="Logo do projeto" />
-        </div>
-        <div class="text-center text-white mt-4">
-          <h1 class="text-2xl font-bold">IFPE Access Control</h1>
-          <p class="text-sm font-medium">
-            Faça o login e tenha acesso as suas atividade no campus!
-          </p>
-        </div>
-      </header>
-
-      <Route path="/">
-        <SignIn />
-      </Route>
-      <Route path="SignIn">
-        <SignIn />
-      </Route>
-      <Route path="SignUp">
-        <SignUp />
-      </Route>
-    </div>
-  </main>
-</Router>
+<main class="bg-gray-800 h-screen flex">
+  <Container/>
+</main>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './Global.css'
+// @ts-ignore
 import App from './App.svelte'
-
+                                                                                                                                                               
 const app = new App({
   target: document.getElementById('app')
 })
