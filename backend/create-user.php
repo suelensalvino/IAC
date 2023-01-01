7d7bac7399448b350ba72f02681b6c41038a17e9
Suelen Salvino <suelensalvinocs@gmail.com>
2023-01-01 19:10:28 -0300

    Modificando back-end cadastro do usuário

--- backend/create-user.php
+++ backend/create-user.php
@@ -1,36 +1,23 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+require 'main.php';
 
-header('Content-Type: application/json');
-
-if(!isset($_POST['nome'])){err('Preencha seu nome', __LINE__);}
-if(!isset($_POST['email'])){err('Preencha seu e-mail', __LINE__);}
-if(!isset($_POST['matricula'])){err('Preencha sua matricula', __LINE__);}
-if(!isset($_POST['resposta'])){err('Preencha sua resposta', __LINE__);}
-if(!isset($_POST['senha'])){err('Preencha sua senha', __LINE__);}
-
-require_once(__DIR__.'/db/database.php');
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$matricula = $_POST['matricula'];
+$resposta = $_POST['resposta'];
+$senha = $_POST['senha'];
 
 try{
     $q = $pdo->prepare('INSERT INTO ALUNO VALUES (null, :nome, :email, :matricula, :resposta, :senha)');
-    $q->bindValue(':nome', $_POST['nome']);
-    $q->bindValue(':email', $_POST['email']);
-    $q->bindValue(':matricula', $_POST['matricula']);
-    $q->bindValue(':resposta', $_POST['resposta']);
-    $q->bindValue(':senha', $_POST['senha']);
-    $q->execute();
-    $userId = $pdo->lastInsertId();
+    $q->execute([
+        'nome' => $nome,
+        'email' => $email,
+        'matricula' => $matricula,
+        'resposta' => $resposta,
+        'senha' => $senha
+    ]);
 }catch(PDOException $ex){
-    err('Erro ao cadastrar usuario', __LINE__);
+    echo 'Erro ao cadastrar usuario';
 }
 
-echo '{"status":1, "message": "usuario criado", "id":"'.$userId.'"}';
-
-function err($message = 'error', $debug = 0){
-    echo '{
-        "status":0,
-        "message":"'.$message.'",
-        "debug":'.$debug.'
-    }';
-    exit();
-}
+?>
--- src/routes/SignUp.svelte
+++ src/routes/SignUp.svelte
@@ -1,99 +1,111 @@
 <script>
-// @ts-nocheck
-
   import Button from "../components/Button.svelte";
-  import { Router, Link } from "svelte-navigator";
+  import { Link } from "svelte-navigator";
 
-  export let user = {};
-
-  const createUser = async () => {
-    const updateRoute = "http://localhost:8000/create-user.php";
-    const data = new FormData();
-    data.append("nome", user.nome);
-    data.append("email", user.email);
-    data.append("matricula", user.matricula);
-    data.append("resposta", user.resposta);
-    data.append("senha", user.senha);
-    let res = await fetch(updateRoute, {
-      method: "POST",
-      body: data,
-    });
-  };
+  let nome = '';
+  let email = '';
+  let matricula = '';
+  let resposta = '';
+  let senha = '';
+  let confSenha = '';
+  
+  function createUser() {
+  if (senha != confSenha) return;
+  let form = new FormData();
+  form.append("nome", nome);
+  form.append("email", email);
+  form.append("matricula", matricula);
+  form.append("resposta", resposta);
+  form.append("senha", senha);
+  fetch('http://localhost:8000/create-user.php', {
+      method: 'POST',
+      body: form
+  })
+}
 </script>
 
-<Router path="SignUp">
-  <form class="flex flex-col gap-2 mx-w-full mt-6" method="post" on:submit|preventDefault={createUser}>
-    <div class="flex flex-col w-full gap-1">
+<form class="flex flex-col gap-2 mx-w-full mt-6" on:submit|preventDefault={createUser}>
+  <div class="flex flex-col w-full gap-1">
       <label for="user" class="text-gray-100">Insira seu nome</label>
-      <input
-        type="text"
-        name="nome"
-        placeholder="Seu nome"
-        required
-        bind:value={user.nome}
-        class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
+      <input 
+      type="text" 
+      name="nome"
+      placeholder="Seu nome"
+      required 
+      bind:value={nome}
+      class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
       />
-    </div>
-    <div class="flex flex-col w-full gap-1">
-      <label for="user" class="text-gray-100"
-        >Insira seu e-mail institucional</label
-      >
-      <input
-        type="email"
-        name="email"
-        placeholder="aluno@ifpe.edu.br.com"
-        required
-        bind:value={user.email}
-        class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
+  </div>
+  <div class="flex flex-col w-full gap-1">
+      <label for="user" class="text-gray-100">Insira seu e-mail institucional</label>
+      <input 
+      type="email"
+      name="email"
+      placeholder="aluno@ifpe.edu.br.com"
+      required
+      bind:value={email}
+      class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
       />
-    </div>
-    <div class="flex flex-col w-full gap-1">
+  </div>
+  <div class="flex flex-col w-full gap-1">
       <label for="user" class="text-gray-100">Insira sua matrícula</label>
-      <input
-        type="text"
-        name="matricula"
-        maxlength="14"
-        placeholder="0000TSIIG0000"
-        required
-        bind:value={user.matricula}
-        class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
+      <input 
+      type="text"
+      name="matricula"
+      maxlength="14"
+      placeholder="0000TSIIG0000"
+      required
+      bind:value={matricula}
+      class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
       />
-    </div>
-    <div class="flex flex-col w-full gap-1">
-      <label for="user" class="text-gray-100"
-        >Qual o nome do seu primeiro animal de estimação?</label
-      >
-      <input
-        type="text"
-        name="resposta"
-        placeholder="Nome do animal"
-        required
-        bind:value={user.resposta}
-        class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
+  </div>
+  <div class="flex flex-col w-full gap-1">
+      <label for="user" class="text-gray-100">Qual o nome do seu primeiro animal de estimação?</label>
+      <input 
+      type="text"
+      name="resposta"
+      placeholder="Nome do animal"
+      required
+      bind:value={resposta}
+      class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
       />
-    </div>
-    <div class="flex flex-col w-full gap-1 mb-8">
+  </div>
+  <div class="flex flex-col w-full gap-1">
       <label for="user" class="text-gray-100">Insira sua senha</label>
-      <input
-        type="password"
-        name="senha"
-        minlength="8"
-        maxlength="20"
-        placeholder="*************"
-        required
-        bind:value={user.senha}
-        class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
+      <input 
+      type="password"
+      name="senha"
+      minlength="8"
+      maxlength="20"
+      placeholder="*************"
+      required
+      bind:value={senha}
+      class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
       />
-    </div>
-    <Button><span slot="button-child">Cadastre-se no sistema</span></Button>
-  </form>
-
-  <footer
-    class="w-full mt-[20px] text-center flex flex-col text-gray-500 underline"
-  >
-    <Link class="hover:text-gray-400" to="/Forget">Esqueceu sua senha?</Link>
-    <Link class="hover:text-gray-400" to="/SignIn"
-      >Já possui uma conta? Entre aqui!</Link
-    >
-  </footer>
-</Router>
+  </div>
+  <div class="flex flex-col w-full gap-1">
+      <label for="user" class="text-gray-100">Confirme sua senha</label>
+      <input 
+      type="password"
+      name="senha"
+      minlength="8"
+      maxlength="20"
+      placeholder="*************"
+      required
+      bind:value={confSenha}
+      class="py-3 px-4 rounded bg-gray-900 text-white placeholder:text-gray-700"
+      />
+  </div>
+  {#if senha != '' && senha != confSenha}
+      <div class="danger">Senhas não conferem</div>
+  {/if}
+  <Button><span slot="button-child">Cadastre-se no sistema</span></Button>
+</form>
+<footer
+class="w-full mt-[20px] text-center flex flex-col text-gray-500 underline"
+>
+<a class="hover:text-gray-400" href="/Forget">Esqueceu sua senha?</a>
+<a class="hover:text-gray-400" href="/SignIn"
+>Já possui uma conta? Entre aqui!</a
+>
+</footer>
