d3b5291faa4e28a25dc93d3d08001030a8e5ff8e
BernardoFOFG <bernardofofg@gmail.com>
2023-01-22 03:19:01 -0300

    feat: paginacao para dashboard

--- src/routes/Dashboard.svelte
+++ src/routes/Dashboard.svelte
@@ -2,8 +2,12 @@
 <script>
   // @ts-nocheck
   import LogoIAC from "../assets/img/Logo_IAC.svg";
+  import Account from "../components/layout/Account.svelte";
+  import Calendario from "../components/layout/Calendario.svelte";
+  import Frequencia from "../components/layout/Frequencia.svelte";
+  import Home from "../components/layout/Home.svelte";
 
-  import { currentPage, isLogged } from "../data-users";
+  import { currentPage, isLogged, dashPage, dashPages } from "../data-users";
   function logout() {
     fetch("http://localhost:8000/logout.php", {
       credentials: "include",
@@ -16,7 +20,7 @@
 <!-- HTML -->
 
 <header
-  class="absolute left-0 w-[300px] h-screen bg-slate-700 flex flex-col items-center "
+  class="absolute left-0 w-[25%] h-screen bg-slate-700 flex flex-col items-center "
 >
   <div
     class="flex flex-col items-center mt-8 mb-12 border-b-2 border-slate-600 w-[90%] pb-4"
@@ -29,22 +33,30 @@
       <li
         class="border-b-2 pb-2 border-green-700 text-slate-50 hover:bg-slate-500 hover:transition-all cursor-pointer px-5 pt-1 "
       >
-        <a href="#">Home</a>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <p id="homeDash" on:click={() => dashPage("home")}>Inicio</p>
       </li>
       <li
         class="border-b-2 pb-2 border-green-700 text-slate-50 hover:bg-slate-500 hover:transition-all cursor-pointer px-2 pt-1 "
       >
-        <a href="#">Frequências</a>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <p id="frequencia" on:click={() => dashPage("frequencia")}>
+          Frequências
+        </p>
       </li>
       <li
         class="border-b-2 pb-2 border-green-700 text-slate-50 hover:bg-slate-500 hover:transition-all cursor-pointer px-2 pt-1 "
       >
-        <a href="#">Calendário</a>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <p id="calendario" on:click={() => dashPage("calendario")}>
+          Calendário
+        </p>
       </li>
       <li
         class="border-b-2 pb-2 border-green-700 text-slate-50 hover:bg-slate-500 hover:transition-all cursor-pointer px-2 pt-1 "
       >
-        <a href="#">Minha conta</a>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <p id="conta" on:click={() => dashPage("conta")}>Minha conta</p>
       </li>
     </ul>
 
@@ -56,3 +68,17 @@
     </button>
   </div>
 </header>
+
+<section class="absolute w-[75%] ml-[35%] mr-[10%] px-12 py-8 h-screen">
+  <div class="w-full bg-slate-700 h-full rounded-lg p-8 flex justify-center items-center ">
+    {#if $dashPages == "home"}
+      <Home />
+    {:else if $dashPages == "frequencia"}
+      <Frequencia />
+    {:else if $dashPages == "calendario"}
+      <Calendario />
+    {:else if $dashPages == "conta"}
+      <Account />
+    {/if}
+  </div>
+</section>
