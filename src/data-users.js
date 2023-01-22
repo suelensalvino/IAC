7c3d56106342e7b5bb20896d02813135440f7af4
BernardoFOFG <bernardofofg@gmail.com>
2023-01-22 03:16:24 -0300

    add: adicionando novo writable

--- src/data-users.js
+++ src/data-users.js
@@ -1,4 +1,13 @@
 import { writable } from "svelte/store";
 
-export const currentPage = writable('index');
+export const currentPage = writable("index");
 export const isLogged = writable(false);
+
+export const dashPages = writable("home");
+
+/**
+ * @param {string} novaDash
+ */
+export function dashPage(novaDash) {
+  dashPages.set(novaDash);
+}
