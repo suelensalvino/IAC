54b83ea2cfc590a110535fde30b306cdea6d649a
BernardoFOFG <bernardofofg@gmail.com>
2023-01-25 16:31:25 -0300

    fix: font-size

--- src/components/layout/Home.svelte
+++ src/components/layout/Home.svelte
@@ -7,7 +7,7 @@
 <!-- HTML -->
 <div class="text-center font-semibold">
   <div class="mb-8">
-    <h1 class="text-2xl  text-slate-100">
+    <h1 class="text-3xl  text-slate-100">
       Bem vindo a Central de Registro Academico
     </h1>
     <h2 class="text-xl text-slate-300">Insira sua presença</h2>
