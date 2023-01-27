18ed218b719f3e039622461f5e334d85ea604694
BernardoFOFG <bernardofofg@gmail.com>
2023-01-27 12:16:30 -0300

    add: validation user

--- backend/validation-create-user.php
+++ backend/validation-create-user.php
@@ -1,9 +1,9 @@
 <?php
 include 'main.php';
 
-$q = $pdo->query("SELECT ALU_EMAIL, ALU_MATRICULA from ALUNO where ALU_MATRICULA != 'null' and ALU_EMAIL != 'null'
+$stmt = $conn->query("SELECT ALU_EMAIL, ALU_MATRICULA from ALUNO where ALU_MATRICULA != 'null' and ALU_EMAIL != 'null'
 
 ");
-$data = $q->fetchAll(PDO::FETCH_ASSOC);
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($data);
